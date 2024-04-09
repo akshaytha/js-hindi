70da7147b61462464ca5fdbaa0026bfe0e49491e
akshaytha <127379591+akshaytha@users.noreply.github.com>
2024-04-09 17:29:19 +0000

    commit changes

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,37 @@
+// "use strict"; // treat all js code as newer version
+
+// //alert(3 +3) // we are using nodejs , not browser
+
+// console.log(3+3)
+// console.log("akshay")
+
+
+// let name = "akshay"
+// let age = 22
+// let isLoggedIn = false
+
+// // number=> 2 to power 53 ye range he 
+// console.log(typeof name);
+
+//let score = "33abc"
+//console.log(typeof score);
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//let someNumber = 44
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// operations...
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+//console.log(str3); 
+
---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,17 @@
+/*const accountId = 144553
+let accountEmail = "akshay.com"
+var accountPassword = " 12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "banguluru"
+
+console.log(accountId);
+
+console.table([accountId,accountPassword,accountCity,accountEmail,accountState])
+*/
+// 
